c17956d0172270c76af1edba72f7dd84760908ea
viktorijaMi <mijalchevaviktorija@gmail.com>
2021-08-19 10:18:31 +0300

    Removed the LoginFilter because of problems with deploy version

--- src/main/java/mk/ukim/finki/icareapp/web/filter/LoginFilter.java
+++ src/main/java/mk/ukim/finki/icareapp/web/filter/LoginFilter.java
@@ -1,40 +1,40 @@
-package mk.ukim.finki.icareapp.web.filter;
-
-import mk.ukim.finki.icareapp.model.User;
-
-import javax.servlet.*;
-import javax.servlet.annotation.WebFilter;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-
-@WebFilter
-public class LoginFilter implements Filter {
-    @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
-
-    }
-
-    @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        HttpServletRequest request = (HttpServletRequest) servletRequest;
-        HttpServletResponse response = (HttpServletResponse) servletResponse;
-
-        User user = (User)request.getSession().getAttribute("user");
-
-        String path = request.getServletPath();
-
-        if (!"/login".equals(path) && !"/images/logo01.png".equals(path) && user==null && !"/".equals(path) && !"/help".equals(path) && !"/register".equals(path) && !"/register/test".equals(path)) {
-            response.sendRedirect("/login");
-        } else {
-            filterChain.doFilter(servletRequest,servletResponse);
-        }
-    }
-
-
-    @Override
-    public void destroy() {
-
-    }
-}
-
+//package mk.ukim.finki.icareapp.web.filter;
+//
+//import mk.ukim.finki.icareapp.model.User;
+//
+//import javax.servlet.*;
+//import javax.servlet.annotation.WebFilter;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import java.io.IOException;
+//
+//@WebFilter
+//public class LoginFilter implements Filter {
+//    @Override
+//    public void init(FilterConfig filterConfig) throws ServletException {
+//
+//    }
+//
+//    @Override
+//    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+//        HttpServletRequest request = (HttpServletRequest) servletRequest;
+//        HttpServletResponse response = (HttpServletResponse) servletResponse;
+//
+//        User user = (User)request.getSession().getAttribute("user");
+//
+//        String path = request.getServletPath();
+//
+//        if (!"/login".equals(path) && !"/images/logo01.png".equals(path) && user==null && !"/".equals(path) && !"/help".equals(path) && !"/register".equals(path) && !"/register/test".equals(path)) {
+//            response.sendRedirect("/login");
+//        } else {
+//            filterChain.doFilter(servletRequest,servletResponse);
+//        }
+//    }
+//
+//
+//    @Override
+//    public void destroy() {
+//
+//    }
+//}
+//
